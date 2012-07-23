e6debc3c65e4cac8ab02d420cc372b2d224d72a1
Roey Yaniv <Roey@ROEY-W7>
2012-07-23 22:58:29 +0300

    Added a main class and a cs class for git testing

---
+++ myClass.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Enyim.Caching;
+using System.Data;
+using OVFM.Data;
+using System.Data.OleDb;
+
+/// <summary>
+/// Summary description for Utils
+/// </summary>
+public static class Utils
+{
+
+    public static MemcachedClient MovieCache;
+    public static MemcachedClient ObjectsCache;
+    public static OVFM.Data.ovfmDB AdsDB;
+
+    public static void InitCache()
+    {
+        MovieCache = new MemcachedClient("enyim.com/memcached");
+        ObjectsCache = new MemcachedClient("enyim.com/memcached");
+    }
+
+    public static void InitDB()
+    {
+        AdsDB = new OVFM.Data.ovfmDB();
+    }
+}
